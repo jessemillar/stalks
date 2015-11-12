92c4d1fb04dba1a08aca85e73f66c9a9c4001ddc
Jesse Millar <hellojessemillar@gmail.com>
2015-11-12 09:32:52 -0700

    Adding environment variables for a testing database connection

--- server.go
+++ server.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
@@ -16,11 +19,11 @@ type Stock struct {
 	ChangePercentYTD float64 `json:"ChangePercentYTD"`
 	ChangeYTD        float64 `json:"ChangeYTD"`
 	High             float64 `json:"High"`
-	LastPrice        float64 `json:"LastPrice"`
+	Price            float64 `json:"LastPrice"`
 	Low              float64 `json:"Low"`
 	MSDate           float64 `json:"MSDate"`
 	MarketCap        int     `json:"MarketCap"`
-	Name             string  `json:Name`
+	Name             string  `json:"Name"`
 	Open             float64 `json:"Open"`
 	Status           string  `json:"Status"`
 	Symbol           string  `json:"Symbol"`
@@ -30,6 +33,19 @@ type Stock struct {
 
 func health(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Uh, we had a slight weapons malfunction, but uh... everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
+}
+
+func testPost(c web.C, w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:"+os.Getenv("TURNIP_PASS")+"@tcp("+os.Getenv("TURNIP_DB")+":"+os.Getenv("TURNIP_PORT"))
+
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s\n", err)
+		return
+	}
+
+	defer db.Close()
+
+	fmt.Fprintf(w, "%s", "Post response")
 }
 
 func price(c web.C, w http.ResponseWriter, r *http.Request) {
@@ -59,11 +75,12 @@ func price(c web.C, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%s: %f\n", stock.Name, stock.LastPrice)
+	fmt.Fprintf(w, "%s: %f\n", stock.Name, stock.Price)
 }
 
 func main() {
 	goji.Get("/health", health)
 	goji.Get("/price/:stock", price)
+	goji.Post("/post", testPost)
 	goji.Serve()
 }
