326c20abc9a6b3ed4123194f0163de33103567ca
Jesse Millar <hellojessemillar@gmail.com>
2015-11-12 20:31:09 -0700

    Starting to add support for getting portfolios

--- server.go
+++ server.go
@@ -13,6 +13,12 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
+type Portfolio struct {
+	UserID   string
+	Ticker   string
+	Quantity int
+}
+
 type Stock struct {
 	Change           float64 `json:"Change"`
 	ChangePercent    float64 `json:"ChangePercent"`
@@ -35,7 +41,7 @@ func health(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Uh, we had a slight weapons malfunction, but uh... everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
 }
 
-func testPost(c web.C, w http.ResponseWriter, r *http.Request) {
+func makeUser(c web.C, w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("mysql", os.Getenv("STALKS_DB_USER")+":"+os.Getenv("STALKS_DB_PASS")+"@tcp("+os.Getenv("STALKS_DB_HOST")+":"+os.Getenv("STALKS_DB_PORT")+")/"+os.Getenv("STALKS_DB_NAME"))
 	defer db.Close()
 	if err != nil { // Die if there was an error
@@ -43,13 +49,30 @@ func testPost(c web.C, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	rows, err := db.Query("INSERT INTO users (firstName, lastName, username, turnips) VALUES (?,?,?,?)", "Jesse", "Millar", "jessemillar", 1000000)
+	rows, err := db.Query("INSERT INTO users (firstName, lastName, userID, username, turnips) VALUES (?,?,?,?,?)", c.URLParams["firstName"], c.URLParams["lastName"], c.URLParams["username"], c.URLParams["username"], 1000000)
 	if err != nil {
 		fmt.Fprintf(w, "Error: %s\n", err)
 	}
 	defer rows.Close()
 
 	fmt.Fprintf(w, "%s", "Success")
+}
+
+func portfolio(c web.C, w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", os.Getenv("STALKS_DB_USER")+":"+os.Getenv("STALKS_DB_PASS")+"@tcp("+os.Getenv("STALKS_DB_HOST")+":"+os.Getenv("STALKS_DB_PORT")+")/"+os.Getenv("STALKS_DB_NAME"))
+	defer db.Close()
+	if err != nil { // Die if there was an error
+		fmt.Fprintf(w, "Error: %s\n", err)
+		return
+	}
+
+	rows, err := db.QueryRow("SELECT * FROM portfolios WHERE userID=?", c.URLParams["userID"]).Scan(&Portfolio)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s\n", err)
+	}
+	defer rows.Close()
+
+	fmt.Fprintf(w, "%s", rows)
 }
 
 func check(c web.C, w http.ResponseWriter, r *http.Request) {
@@ -83,9 +106,9 @@ func check(c web.C, w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	goji.Get("/health", health)
-	// goji.Get("/portfolio/:user", portfolio)
+	goji.Get("/portfolio/:userID", portfolio)
 	goji.Get("/check/:stock", check)
-	goji.Post("/post", testPost)
+	goji.Post("/makeUser/:username/:firstName/:lastName", makeUser) // Mostly for development purposes
 	// goji.Post("/buy/:stock/:quantity", buy)
 	// goji.Post("/sell/:stock/:quantity", sell)
 	goji.Serve()
