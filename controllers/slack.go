f572ec84dcbb5472ccebee98a06d42c84df7aa99
Jesse Millar <hellojessemillar@gmail.com>
2015-11-16 14:13:08 -0700

    Adding command shortcuts because I'm lazy when testing in Slack

--- controllers/slack.go
+++ controllers/slack.go
@@ -10,13 +10,13 @@ import (
 func Slack(c web.C, w http.ResponseWriter, r *http.Request) {
 	params := strings.Fields(r.PostFormValue("text"))
 
-	if params[0] == "check" {
+	if params[0] == "check" || params[0] == "c" {
 		Check(c, w, r)
-	} else if params[0] == "portfolio" {
+	} else if params[0] == "portfolio" || params[0] == "p" {
 		Portfolio(c, w, r)
-	} else if params[0] == "buy" {
+	} else if params[0] == "buy" || params[0] == "b" {
 		Buy(c, w, r)
-	} else if params[0] == "sell" {
+	} else if params[0] == "sell" || params[0] == "s" {
 		Sell(c, w, r)
 	}
 }
