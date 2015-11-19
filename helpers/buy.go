aa42599abebbc1e4f667e8e657d1ec9a1eee82e0
Jesse Millar <hellojessemillar@gmail.com>
2015-11-19 08:49:03 -0700

    Webhook purchase announcements work

--- helpers/buy.go
+++ helpers/buy.go
@@ -9,11 +9,11 @@ import (
 
 func Buy(userID string, quantity int, symbol string) string {
 	stock := models.CheckStock(symbol)
-	turnips := models.GetUser(userID).Turnips
+	user := models.GetUser(userID)
 
 	// Make sure they have enough turnips to buy
-	if turnips < stock.Price*quantity {
-		return fmt.Sprintf("%d shares of %s costs %s turnips and you have %s turnips.\n", quantity, strings.ToUpper(symbol), Comma(stock.Price*quantity), Comma(turnips)) // Return information about a user's portfolio
+	if user.Turnips < stock.Price*quantity {
+		return fmt.Sprintf("%s shares of %s costs %s turnips and you have %s turnips.\n", Comma(quantity), strings.ToUpper(symbol), Comma(stock.Price*quantity), Comma(user.Turnips)) // Return information about a user's portfolio
 	}
 
 	models.SubtractTurnips(userID, stock.Price*quantity)
@@ -24,5 +24,7 @@ func Buy(userID string, quantity int, symbol string) string {
 
 	models.AddShares(userID, symbol, quantity)
 
-	return fmt.Sprintf("%s turnips were spent to add %d share(s) of %s to your portfolio.\n", Comma(stock.Price*quantity), quantity, strings.ToUpper(symbol)) // Return information about a user's portfolio
+	Webhook(fmt.Sprintf("<@%s|%s> purchased %s share(s) of %s for %s turnips.", user.UserID, user.Username, Comma(quantity), strings.ToUpper(symbol), Comma(stock.Price*quantity)))
+
+	return fmt.Sprintf("%s turnips were spent to add %s share(s) of %s to your portfolio.\n", Comma(stock.Price*quantity), Comma(quantity), strings.ToUpper(symbol)) // Return information about a user's portfolio
 }
--- helpers/sell.go
+++ helpers/sell.go
@@ -16,8 +16,10 @@ func Sell(userID string, quantity int, symbol string) string {
 		models.SubtractShares(userID, symbol, quantity)
 		models.AddTurnips(userID, stock.Price*quantity) // Add turnips to our wallet
 	} else { // Else return a human-readable error
-		return fmt.Sprintf("You do not have enough shares of %s to sell %d. You have %d shares.\n", strings.ToUpper(symbol), quantity, holdings) // Return information about a user's portfolio
+		return fmt.Sprintf("You do not have enough shares of %s to sell %s. You have %s shares.\n", strings.ToUpper(symbol), Comma(quantity), Comma(holdings)) // Return information about a user's portfolio
 	}
 
-	return fmt.Sprintf("%d share(s) of %s have been sold for a total of %s turnips.\n", quantity, strings.ToUpper(symbol), Comma(quantity*stock.Price)) // Return information about a user's portfolio
+	Webhook(fmt.Sprintf("<@%s|%s> sold %s share(s) of %s for %s turnips.", user.UserID, user.Username, Comma(quantity), strings.ToUpper(symbol), Comma(stock.Price*quantity)))
+
+	return fmt.Sprintf("%s share(s) of %s have been sold for a total of %s turnips.\n", Comma(quantity), strings.ToUpper(symbol), Comma(quantity*stock.Price)) // Return information about a user's portfolio
 }
